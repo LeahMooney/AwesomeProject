43ce50fae12a33d9800c14f7aaf98c37296cb013
LeahMooney <lmm204@pitt.edu>
2019-05-24 18:15:55 -0400

    New branch Leah

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Greeting name="Leah" collegeName="University of Pittsburgh"/>
+        <Greeting name="Leah Mooney" collegeName="University of Pittsburgh"/>
       </View>
 
     );
